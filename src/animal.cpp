8b164bc523344f960b69a0b9f97ea2fc5da8e6ea
Quentin18 <quentindeschamps18@orange.fr>
2021-01-06 19:50:22 +0100

    add animal documentation

--- src/animal.cpp
+++ src/animal.cpp
@@ -1,3 +1,7 @@
+/**
+ * @file animal.cpp
+ * @brief Source code for Animal class
+ */
 #include <cmath>
 
 #include "animal.hpp"
@@ -5,18 +9,30 @@
 #include "perlin.hpp"
 #include "distributions.hpp"
 
-/** Re-maps a number from one range to another.
+/**
+ * Re-maps a number from one range to another.
  * 
  * Inspired by https://processing.org/reference/map_.html
+ * 
+ * @param value the incoming value to be converted
+ * @param start1 lower bound of the value's current range
+ * @param stop1 upper bound of the value's current range
+ * @param start2 lower bound of the value's target range
+ * @param stop2 upper bound of the value's target range
+ * @return float
+ * 
  */
 float Animal::map(float value, float start1, float stop1, float start2, float stop2)
 {
     return start2 + (stop2 - start2) * (value - start1) / (stop1 - start1);
 }
 
+/**
+ * Animal constructor
+ */
 Animal::Animal() : radius(ANIMAL_DEFAULT_RADIUS), hp(ANIMAL_MAX_HP)
 {
-    // Set variables
+    // Set time variables
     tx = TIME_DISTRIBUTION(RANDOM_ENGINE);
     ty = TIME_DISTRIBUTION(RANDOM_ENGINE);
 
@@ -29,8 +45,16 @@ Animal::Animal() : radius(ANIMAL_DEFAULT_RADIUS), hp(ANIMAL_MAX_HP)
     this->setPosition(x, y);
 }
 
+/**
+ * Animal destructor
+ */
 Animal::~Animal() {}
 
+/**
+ * Move the animal using Perlin noise
+ * 
+ * @param timeSpeed speed of the simulation
+ */
 void Animal::move(const float timeSpeed)
 {
     x = map(Perlin::noise(tx), -1, 1, 0, WINDOW_WIDTH);
@@ -41,16 +65,31 @@ void Animal::move(const float timeSpeed)
     hp -= timeSpeed;
 }
 
+/**
+ * Return true if the animal is dead
+ * 
+ * @return true if the animal has no health points
+ */
 bool Animal::isDead() const
 {
     return hp <= 0;
 }
 
+/**
+ * Reset the health points of the animal
+ */
 void Animal::resetHp()
 {
     hp = ANIMAL_MAX_HP;
 }
 
+/**
+ * Return the distance between two points of the window
+ * 
+ * @param a vector representing the first point
+ * @param b vector representing the second point
+ * @return Euclidean distance between the two points
+ */
 float Animal::distance(const sf::Vector2f a, const sf::Vector2f b)
 {
     return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
--- src/animal.hpp
+++ src/animal.hpp
@@ -1,14 +1,21 @@
+/**
+ * @file animal.hpp
+ * @brief Header file for Animal class
+ */
 #pragma once
 
 #include <SFML/Graphics.hpp>
 
+/**
+ * Represent an animal
+ */
 class Animal : public sf::CircleShape
 {
 private:
     float radius;
-    float x, y;
+    float x, y;     // position
     float tx, ty;   // time variables
-    float hp;   // health points
+    float hp;       // health points
     static float map(float value, float start1, float stop1, float start2, float stop2);
 public:
     Animal();
