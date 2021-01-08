81f51cc6567fa02e36edb45a7c902ace8acd1163
Quentin18 <quentindeschamps18@orange.fr>
2021-01-08 17:21:14 +0100

    add test distributions

--- tests/TestCase.cpp
+++ tests/TestCase.cpp
@@ -8,6 +8,7 @@
 #include "animals.hpp"
 #include "rabbit.hpp"
 #include "foods.hpp"
+#include "distributions.hpp"
 
 TEST_CASE("1: Tests on Animals")
 {
@@ -26,6 +27,16 @@ TEST_CASE("2: Tests on Foods")
     Foods foods;
     foods.init(10);
     REQUIRE(foods.getList()->size() == 10);
+}
+
+TEST_CASE("3: Random distribution")
+{
+    float n;
+    for (size_t i = 0; i < 100; i++)
+    {
+        n = RAND_DISTRIBUTION(RANDOM_ENGINE);
+        REQUIRE((n >= 0 && n <= 1));
+    }
 }
 
 // TODO to complete
