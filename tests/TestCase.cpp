464db26d0550fe28ccde9e7386ee2604e31f72e8
Quentin18 <quentindeschamps18@orange.fr>
2021-01-08 17:18:17 +0100

    add tests on Foods

--- tests/TestCase.cpp
+++ tests/TestCase.cpp
@@ -1,9 +1,13 @@
 // Let Catch provide main():
 #define CATCH_CONFIG_MAIN
 
+#include <list>
+
 #include "catch.hpp"
+
 #include "animals.hpp"
 #include "rabbit.hpp"
+#include "foods.hpp"
 
 TEST_CASE("1: Tests on Animals")
 {
@@ -15,6 +19,13 @@ TEST_CASE("1: Tests on Animals")
     rabbits.init(30);
     REQUIRE(rabbits.getNb() == 30);
     REQUIRE_FALSE(rabbits.empty());
+}
+
+TEST_CASE("2: Tests on Foods")
+{
+    Foods foods;
+    foods.init(10);
+    REQUIRE(foods.getList()->size() == 10);
 }
 
 // TODO to complete
