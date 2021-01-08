d5366f08026bbbf232ca6b6088bc463eadf15d0b
Quentin18 <quentindeschamps18@orange.fr>
2021-01-08 17:15:29 +0100

    add test on Animals

--- tests/TestCase.cpp
+++ tests/TestCase.cpp
@@ -2,5 +2,19 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
+#include "animals.hpp"
+#include "rabbit.hpp"
+
+TEST_CASE("1: Tests on Animals")
+{
+    Animals<Rabbit> rabbits;
+    REQUIRE(rabbits.empty());
+    REQUIRE(rabbits.getNb() == 0);
+    REQUIRE(rabbits.getNbEaten() == 0);
+    REQUIRE(rabbits.getNbBirths() == 0);
+    rabbits.init(30);
+    REQUIRE(rabbits.getNb() == 30);
+    REQUIRE_FALSE(rabbits.empty());
+}
 
 // TODO to complete
